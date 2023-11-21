56f23255301e39875b2b5ce2d45fa0e3c82270f6
Dabbott2005 <dustinabbott05@gmail.com>
2023-11-20 22:29:05 -0600

    updated

--- roadrunner23-24/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/auto/redRightRR.java
+++ roadrunner23-24/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/auto/redRightRR.java
@@ -506,7 +506,7 @@ public class redRightRR extends LinearOpMode {
                 telemetry.addData("Object Position", "Right");
 
                 currentState = State.TRAJ_RIGHT;
-                // Perform actios for the object in the middle.
+                // Perform actions for the object in the middle.
                 // Example: drive forward or execute middle-specific commands.
             }   // end for() loop
 
