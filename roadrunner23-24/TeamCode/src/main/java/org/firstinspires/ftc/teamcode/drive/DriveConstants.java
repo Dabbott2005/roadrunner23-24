a995e30f88c875cb159dab9499448ac199223809
robocats15877 <chsrobocats@gmail.com>
2023-11-28 14:17:21 -0600

    update k values

--- roadrunner23-24/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ roadrunner23-24/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -54,9 +54,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
-    public static double kA = 0;
-    public static double kStatic = 0;
+    public static double kV =  0.016;
+    public static double kA =  0.0015;
+    public static double kStatic = 0.08669;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
@@ -66,7 +66,7 @@ public class DriveConstants {
      * inches.
      */
     public static double MAX_VEL = 43;
-    public static double MAX_ACCEL = 45;
+    public static double MAX_ACCEL = 26;
     public static double MAX_ANG_VEL = Math.toRadians(60);
     public static double MAX_ANG_ACCEL = Math.toRadians(60);
 
